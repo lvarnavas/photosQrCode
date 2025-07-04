9f7c6c63aae8a09fc7eb82ff1cacd75df78f75d0
Lampros Varnavas <lamprosvarnavas@Mac.home>
2025-07-04 15:43:13 +0300

    fix: event name

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const EVENT_NAME = 'alex-maria-wedding';
+const EVENT_NAME = 'dimitris-matoula-wedding';
 
 export default function Gallery() {
   const [images, setImages] = useState([]);
